c9b88d5be5772c19245fe30809992e7ef948b57d
wilson <ggpeal@gmail.com>
2024-11-29 23:28:45 -0500

    feat: crud dev

--- health-progress-backend/server.js
+++ health-progress-backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const healthRoutes = require('./routes/healthRoutes');
+const patientCrud = require('./routes/patientCrud');
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Routes
 app.use('/api/health', healthRoutes);
+app.use('/api/patient_crud', patientCrud);  //patient_crud api
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
