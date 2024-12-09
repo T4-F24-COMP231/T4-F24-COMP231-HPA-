7ad1a50faaf30029bdcae4d4652e7beef5a10e90
wilson <ggpeal@gmail.com>
2024-12-08 21:16:42 -0500

    fix: two api uri

--- health-progress-backend/server.js
+++ health-progress-backend/server.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 const healthRoutes = require('./routes/HealthRoutes');
 const wearableRoutes = require('./routes/wearablesRoutes')
 const healthMetricsRouter = require('./routes/healthMetrics');
-const healthRoutes = require('./routes/healthRoutes');
+// const healthRoutes = require('./routes/healthRoutes');
 const patientCrud = require('./routes/patientCrud');
-const healthMetrics = require('./routes/healthMetrics');
-const {forTest} = require("./controllers/patientCrudController");
+const healthMetrics = require('./routes/healthMetrics.js');
+// const {forTest} = require("./controllers/patientCrudController");
 
 dotenv.config();
 
@@ -21,14 +21,16 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error('Database connection failed:', err));
 
+//curd and health_metrics
+app.use('/apii/patient_crud', patientCrud);  //patient_crud api
+app.use('/apii/health_metrics', healthMetrics);  //health_metrics api
+
 // Routes
 app.use('/health', healthRoutes);
 app.use('/api', wearableRoutes);
 app.use('/api', healthMetricsRouter);
 
-app.use('/api/health', healthRoutes);
-app.use('/api/patient_crud', patientCrud);  //patient_crud api
-app.use('/api/health_metrics', healthMetrics);  //health_metrics api
+
 
 // app.get('/test', forTest);
 
